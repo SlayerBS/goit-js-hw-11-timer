1e546e347a035b8334b7cd27ac6a38adb0873c8c
Sergii Bogush <sergbogush@gmail.com>
2021-05-20 20:10:04 +0300

    fix some files

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ const refs = {
   daysField: document.querySelector('[data-value="days"]'),
 };
 
-getNumbers = function (time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  return [days, hours, mins, secs];
-};
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
 
-setDate = function (year, month, day) {
-  return new Date(year, month, day);
-};
+  start() {
+    this.timerId = setInterval(() => {
+      const time = this.targetDate - new Date();
+      const numbers = this.getNumbers(time);
+      refs.secondsField.innerText = numbers[3];
+      refs.minutesField.innerText = numbers[2];
+      refs.hoursField.innerText = numbers[1];
+      refs.daysField.innerText = numbers[0];
+    }, 1000);
+  }
 
-const targetDate = setDate(2022, 01, 02);
+  getNumbers = time => {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return [days, hours, mins, secs];
+  };
+}
 
-timerId = setInterval(() => {
-  const time = targetDate - new Date();
-  const numbers = getNumbers(time);
-  console.log(numbers);
-  refs.secondsField.innerText = numbers[3];
-  refs.minutesField.innerText = numbers[2];
-  refs.hoursField.innerText = numbers[1];
-  refs.daysField.innerText = numbers[0];
-}, 1000);
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 25, 2021'),
+});
+
+timer.start();
